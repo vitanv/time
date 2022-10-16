106cb8fc035ce4989aef8afa07c5beb35ff01b25
vitanv <vitanvlad20@gmail.com>
2022-10-16 19:07:51 +0100

    Added time and date

--- src/Clock.js
+++ src/Clock.js
@@ -1,15 +1,62 @@
 import React from 'react';
 import './Clock.css';
 
+
+const day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 export class Clock extends React.Component{
   constructor(props){
     super(props);
-    this.state = {}
+    
+    this.state = {
+      date:new Date,
+    }
   }
+
+  componentDidMount() {
+    this.timer = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick = () => {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render(){
     return(
       <div>
-        Clock
+        <div className='time'>{this.state.date.toLocaleTimeString()}</div>
+        <div className='time'>{this.state.date.toLocaleTimeString('en-UK', { hour12: true })}</div>
+        <div className='date'>{day[this.state.date.getDay()]} {this.state.date.getDate()} of {month[this.state.date.getDay()]} {this.state.date.getUTCFullYear()}</div>
       </div>
     )
   }
