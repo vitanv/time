36f36cb943d5d0a1d6415906485a7020cac23eb6
vitanv <vitanvlad20@gmail.com>
2022-10-23 21:46:25 +0100

    Using existing functions created a pomodoro clock

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Pomodoro.css';
+import { format } from './format';
 
 export class Pomodoro extends React.Component{
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      active: false,
+      minutes:25,
+      seconds:0,
+    }
+    this.changeState = this.changeState.bind(this);
   }
 
   format = (value) =>{
@@ -14,11 +20,55 @@ export class Pomodoro extends React.Component{
       return "" + value;
     }
   }
-  
+
+  changeState = () =>{
+    if(this.state.active){
+      clearInterval(this.timer);
+    }else{
+      let minute = this.state.minutes;
+      let second = this.state.seconds;
+      
+      this.timer = setInterval( () => {
+        second--;
+        if(second < 0 && minute >= 1){
+          minute--;
+          second = 59;
+        }else if(second <= 0 && minute <= 0){
+          this.setState({
+            active:false,
+          });
+        }
+        if(this.state.active == false) return;
+        this.setState({
+          minutes:minute,
+          seconds:second,
+        });
+      }, 1000);
+    }
+    this.setState({
+      active:!this.state.active,
+    });
+  }
+
+  resetWatch = () =>{
+    this.setState({
+      minutes:25,
+      seconds:0,
+    });
+  }
+
   render(){
     return(
-      <div>
-        Pomodoro
+      <div className='container'>
+        <div className='display'>
+          <span>{format(this.state.minutes)}</span>:
+          <span>{format(this.state.seconds)}</span>
+        </div>
+        <label className="switch">
+          <input type="checkbox"  />
+          <span className="slider" onClick={() => this.changeState()} />
+        </label>
+        <button className='btn' onClick={() => this.resetWatch()}>Reset</button>
       </div>
     )
   }
